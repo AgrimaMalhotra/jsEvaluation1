f3597ed180949083cdeea1ab71b21a784f017595
Agrima <er.agrima.malhotra@gmail.com>
2023-01-19 00:26:53 +0530

    feat: add one game score

--- bowlingScore.js
+++ bowlingScore.js
@@ -1,62 +1,60 @@
-function calculateScore(roundScore) {
-    let index = 0;
-    let eachRoundScore, totalScore = 0;
-
-    while (index < 10) {
-        eachRoundScore = roundScore[index].reduce((acc, num) => acc + num, 0);
-
-        if (eachRoundScore <= 10) {//open round
-            totalScore += eachRoundScore;
-        }
-        else if (roundScore[index][0] === 10 && roundScore[index + 1][0] === 10) {
-            //consecutive strike
-            totalScore += 20 + roundScore[index + 1][0];
-        }
-        else if (roundScore[index][0] === 10 && roundScore[index + 1][0] !== 10) {
-            //strike
-            totalScore += 10 + roundScore[index + 1][0] + roundScore[index + 1][0];
-        }
-        else if (roundScore[index][1] === 10) {
-            //spare
-            totalScore += 10 + roundScore[index + 1][0];
-        }
-
-        index += 1;
-
+let getStrikeScore = (frameScore, index) => {
+    let strikeScore = 0;
+    if (frameScore[index + 1][0] === 10) {
+        //consecutive strike
+        strikeScore = 20 + frameScore[index + 2][0];
     }
-    return totalScore;
+    else {
+        //one strike
+        strikeScore = 10 + frameScore[index + 1][0] + frameScore[index + 1][1];
+    }
+    return strikeScore;
+}
+let getFrameScore = (frameScore, index) => {
+    let eachFrameScore = frameScore[index].reduce((acc, num) => acc + num, 0);
+    if (eachFrameScore < 10) {
+        //open round
+        return eachFrameScore;
+    }
+    else if (frameScore[index][0] === 10) { //strike
+        return (getStrikeScore(frameScore, index));
+    }
 
+    else if (eachFrameScore === 10) {
+        //spare
+        return (10 + frameScore[index + 1][0]);
+    }
 }
 
-function score(rolls) {
-    if (!Array.isArray(rolls) || rolls.length === 0) {
-        throw new Error('Invalid array');
-    }
-    console.log(rolls.length)
-    let roundScore = {};
+let getFrames = rolls => {
+    var roundScore = {}, key = 0;
     for (let index = 0; index < rolls.length;) {
-        if (Object.keys(roundScore).length === 9) {
-            roundScore[9] = rolls.slice(index,);
-            break;
-        }
         if (rolls[index] === 10) {
-            roundScore[index] = [rolls[index]];
+            roundScore[key] = [rolls[index]];
             index++;
         }
         else {
-            roundScore[parseInt(index / 2)] = rolls.slice(index, index + 2);
+            roundScore[key] = rolls.slice(index, index + 2);
             index += 2;
         }
+        key++;
     }
-    if (Object.keys(roundScore).length != 10) {
-        throw new Error('Invalid');
-    }
-
-    return calculateScore(roundScore);
+    return roundScore;
 }
 
-var rolls = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
-// var rolls=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10];
-console.log(score(rolls));
+let getGameScore = rolls => {
+    let index = 0, totalScore = 0;
+    if (!Array.isArray(rolls) || rolls.length === 0) {
+        throw new Error('Invalid array');
+    }
+    var frameScore = getFrames(rolls);
+    if (Object.keys(frameScore).length <= 10) {
+        throw new Error('Invalid');
+    }
+    while (index < 10) {
+        totalScore += getFrameScore(frameScore, index);
+        index++;
+    }
+    return totalScore;
+}
 
-module.exports = { score };
